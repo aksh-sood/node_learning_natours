911bb8390297e97a32a1c47cbbb9d7d07701d6de
Aksh Sood <sood_aksh_2001@outlook.com>
2021-12-27 20:17:59 +0530

    atlas database connected

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
 const app = require("./app");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log("DB connected succesfully");
+  });
+
 console.log(process.env);
 const port = process.env.PORT;
 app.listen(port, () => {
