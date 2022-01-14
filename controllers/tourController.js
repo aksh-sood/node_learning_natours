72c811b983a8a023a1697f18cc3f38a99f42e435
Aksh Sood <sood_aksh_2001@outlook.com>
2022-01-14 10:07:19 +0530

    aliassing

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -20,6 +20,13 @@ const mongoose = require("mongoose");
 //   }
 //   next();
 // };
+exports.aliasTopTours = async (req, res,next) => {
+  req.query.limit="5";
+  req.query.sort="-ratingsAverage,price";
+  req.query.fields="name,price,ratingsAverage,summary,difficulty";
+  next();
+}
+
 
 exports.getAllTours = async (req, res) => {
   try {
