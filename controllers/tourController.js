f1a45b84c82af72ff006097974a8a4b2a848d42e
Aksh Sood <sood_aksh_2001@outlook.com>
2022-01-15 07:54:48 +0530

    refactor 2 for 100th video

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,5 @@
 const Tour = require("./../models/tourModel");
-const mongoose = require("mongoose");
+const APIFeatures = require("./../utils/apiFeatures");
 // const tours = JSON.parse(
 //   fs.readFileSync(`4-natours/starter/dev-data/data/tours-simple.json`)
 // );
@@ -21,58 +21,6 @@ const mongoose = require("mongoose");
 //   next();
 // };
 
-class APIFeatures {
-  constructor(query, queryString) {
-    this.query = query;
-    this.queryString = queryString;
-  }
-
-  filter() {
-    const queryObj = { ...this.queryString };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach(el => delete queryObj[el]);
-
-    // 1B) Advanced filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-
-    this.query = this.query.find(JSON.parse(queryStr));
-
-    return this;
-  }
-
-  sort() {
-    if (this.queryString.sort) {
-      const sortBy = this.queryString.sort.split(',').join(' ');
-      this.query = this.query.sort(sortBy);
-    } else {
-      this.query = this.query.sort('-createdAt');
-    }
-
-    return this;
-  }
-
-  limitFields() {
-    if (this.queryString.fields) {
-      const fields = this.queryString.fields.split(',').join(' ');
-      this.query = this.query.select(fields);
-    } else {
-      this.query = this.query.select('-__v');
-    }
-
-    return this;
-  }
-
-  paginate() {
-    const page = this.queryString.page * 1 || 1;
-    const limit = this.queryString.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-
-    this.query = this.query.skip(skip).limit(limit);
-
-    return this;
-  }
-}
 
 exports.aliasTopTours = async (req, res,next) => {
   req.query.limit="5";
@@ -83,7 +31,7 @@ exports.aliasTopTours = async (req, res,next) => {
 
 
 exports.getAllTours = async (req, res) => {
-  // try {
+  try {
    
     // //FILTERING
     // const queryObj = { ...req.query };
@@ -139,9 +87,9 @@ exports.getAllTours = async (req, res) => {
         tours: tours,
       },
     });
-  // } catch (err) {
-  //   res.status(404).json({ status: "fail", message: err })
-  // }
+  } catch (err) {
+    res.status(404).json({ status: "fail", message: err })
+  }
 };
 
 exports.getTour = async (req, res) => {
