8d1034d900da1bf7d64de6746b63b81b432bd521
Aksh Sood <sood_aksh_2001@outlook.com>
2022-02-25 13:19:00 +0530

    error controller

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require("express");
 const morgan = require("morgan");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
+const AppError= require("./utils/appError");
+const globalErrorHandler=require("./controllers/errorController");
 
 const app = express();
+
 app.use(morgan("dev"));
 app.use(express.static(`4-natours/starter/public/`));
 
@@ -25,5 +28,15 @@ app.use("/api/v1/tours", tourRouter);
 // app.post("/api/v1/tours",createTour);
 // app.patch("/api/v1/tours/:id", updateTour);
 // app.delete("/api/v1/tours/:id",deleteTour);
+
+app.all("*",(req,res,next) => {
+  // const err=new Error(`Can't find ${req.originalUrl} on this server!`);
+  // err.statusCode=404;
+  // err.status="Fail";
+  
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`,404));
+})
+
+app.use(globalErrorHandler)
 
 module.exports = app;
---
+++ controllers/errorController.js
@@ -0,0 +1,9 @@
+
+module.exports=(err,req,res,next) => {
+    // console.log(err.stack);
+    err.statusCode = res.statusCode||500;
+    err.status = "Error";
+    res.status(err.statusCode).json({
+      status:err.status,
+      message:err.message
+    })};
